108d7a5811ba94346c4b01efff94325c003a98c6
duq <1184116431@qq.com>
2017-05-31 20:28:19 +0800

    test

---
+++ src/main/java/springBean/TestIoc.java
@@ -0,0 +1,12 @@
+package springBean;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by duq on 2017/5/31.
+ */
+@Service
+public class TestIoc {
+
+
+}
---
+++ src/test/java/testBean/TestIoc.java
@@ -0,0 +1,11 @@
+package testBean;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by duq on 2017/5/31.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestIoc {
+}
